8ec9579fdaad2d078ee010a68b960e51943d7f04
Mayonezyck <yz8733@rit.edu>
2026-02-22 23:06:42 -0500

    Initial commit

---
+++ source/SceneFactory/SceneFactory/tasks/direct/scenefactory_marl/__init__.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+
+gym.register(
+    id="Template-Scenefactory-Marl-Direct-v0",
+    entry_point=f"{__name__}.scenefactory_marl_env:ScenefactoryMarlEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.scenefactory_marl_env_cfg:ScenefactoryMarlEnvCfg",
+        "skrl_ippo_cfg_entry_point": f"{agents.__name__}:skrl_ippo_cfg.yaml",
+    },
+)
